2a946a1cad9d689eb5d0f2f7230695cdcc972dd6
PhuNguyen <ndp.uit89@yahoo.com>
2020-04-28 22:30:54 +0700

    Adds Mobile Data Usage Sample

---
+++ app/src/main/java/com/sphtech/mobiledatausage/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.sphtech.mobiledatausage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setTitle(R.string.app_label)
+    }
+}
